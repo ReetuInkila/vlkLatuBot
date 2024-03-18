cddc09b52c1528811b0b00e52e9f223b37273dc1
lrtinkiv <lrtinkiv@jyu.fi>
2024-03-18 15:49:22 +0200

    uimahalli added as location

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ bot.command('help', ctx => {
     console.log(ctx.from)
     bot.telegram.sendMessage(ctx.chat.id, `Käytettävissä olevat komennot.
     /help
+    /uimahalli
     /voimailusali
     /wareena`, {})
+})
+
+bot.command('uimahalli', ctx => {
+    console.log(ctx.from)
+    const today = new Date()
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    axios.get(`https://valkeakoski.tilamisu.fi/fi/locations/835/reservations.json?from=${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}&to=${tomorrow.getFullYear()}-${tomorrow.getMonth()+1}-${tomorrow.getDate()}`)
+    .then(response => {
+        const calendar = makeCalendar(response.data)
+        bot.telegram.sendMessage(ctx.chat.id, calendar, {})
+    })
 })
 
 bot.command('voimailusali', ctx => {
@@ -23,7 +36,6 @@ bot.command('voimailusali', ctx => {
     tomorrow.setDate(tomorrow.getDate() + 1)
     axios.get(`https://valkeakoski.tilamisu.fi/fi/locations/852/reservations.json?from=${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}&to=${tomorrow.getFullYear()}-${tomorrow.getMonth()+1}-${tomorrow.getDate()}`)
     .then(response => {
-        //console.log(response.data)
         const calendar = makeCalendar(response.data)
         bot.telegram.sendMessage(ctx.chat.id, calendar, {})
     })
@@ -36,11 +48,12 @@ bot.command('wareena', ctx => {
     tomorrow.setDate(tomorrow.getDate() + 1)
     axios.get(`https://valkeakoski.tilamisu.fi/fi/locations/836/reservations.json?from=${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}&to=${tomorrow.getFullYear()}-${tomorrow.getMonth()+1}-${tomorrow.getDate()}`)
     .then(response => {
-        //console.log(response.data)
         const calendar = makeCalendar(response.data)
         bot.telegram.sendMessage(ctx.chat.id, calendar, {})
     })
 })
+
+
 
 function makeCalendar(data){
     const date = new Date(data[0].start_date).toLocaleDateString()
